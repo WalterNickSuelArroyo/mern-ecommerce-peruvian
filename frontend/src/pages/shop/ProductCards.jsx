c4b547b2bb5375d4d60260da836bf97722cd38bd
WalterNickSuelArroyo <nicodehero21@outlook.com>
2024-09-07 23:51:15 -0500

    pagina de cada producto singular

--- frontend/src/pages/shop/ProductCards.jsx
+++ frontend/src/pages/shop/ProductCards.jsx
@@ -2,14 +2,13 @@ import { Link } from "react-router-dom";
 import RatingStarts from "../../components/RatingStarts";
 
 const ProductCards = ({products = []}) => {
-    console.log(products);
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {
             products.map((product, index) => (
                 <div key={index} className="product__card">
                     <div className="relative">
-                        <Link to={`/shop/${product._id}`}>
+                        <Link to={`/shop/${product.id}`}>
                             <img src={product.image} alt="product image" className="max-h-96 md:h-64 w-full object-cover hover:scale-105 transition-all duration-300" />
                         </Link>
                         <div className="hover:block absolute top-3 right-3">
