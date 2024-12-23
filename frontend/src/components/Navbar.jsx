302c3aa6d705ca2bdb618884cd528bb3e8a4925b
WalterNickSuelArroyo <nicodehero21@outlook.com>
2024-12-22 21:33:58 -0500

    navbar dropdown menus

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,57 @@
 import { useState } from "react";
-import { useSelector } from "react-redux"
-import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { Link, useNavigate } from "react-router-dom"
 import CartModal from "../pages/shop/CartModal";
+
+import avatarImg from "../assets/avatar.png";
+import { useLogoutUserMutation } from "../redux/features/auth/authApi";
+import { logout } from "../redux/features/auth/authSlice";
 
 const Navbar = () => {
     const products = useSelector((state) => state.cart.products);
     const [isCartOpen, setisCartOpen] = useState(false);
     const handleCartToggle = () => {
         setisCartOpen(!isCartOpen);
+    }
+
+    //show user if logged in
+    const dispatch = useDispatch();
+    const {user} = useSelector((state) => state.auth);
+    const [logoutUser] = useLogoutUserMutation()
+    const navigate = useNavigate();
+
+    //drodown menus
+    const [IsDropDownOpen, setIsDropDownOpen] = useState(false);
+    const handDropDownToggle = () => {
+        setIsDropDownOpen(!IsDropDownOpen);
+    }
+
+    //admin drowdonw menus
+    const adminDropDownMenus = [
+        {label: "Dashboard", path: "/dashboard/admin"},
+        {label: "Manage Items", path: "/dashboard/manage-products"},
+        {label: "All Orders", path: "/dashboard/manage-orders"},
+        {label: "Add New Post", path: "/dashboard/add-new-post"},
+    ]
+
+    //user drowdonw menus
+    const userDropDownMenus = [
+        {label: "Dashboard", path: "/dashboard"},
+        {label: "Profile", path: "/dashboard/profile"},
+        {label: "Payments", path: "/dashboard/payments"},
+        {label: "Orders", path: "/dashboard/orders"},
+    ]
+
+    const dropdownMenus = user?.role === 'admin' ? [...adminDropDownMenus] : [...userDropDownMenus]
+
+    const handleLogout = async() => {
+        try {
+            await logoutUser().unwrap();
+            dispatch(logout())
+            navigate('/')
+        } catch (error) {
+            console.error("Failed to log out", error)
+        }
     }
 
   return (
@@ -39,9 +83,33 @@ const Navbar = () => {
                     </button>
                 </span>
                 <span>
-                    <Link to="login">
-                        <i className="ri-user-line"></i>
-                    </Link>
+                    {
+                        user && user ? (<>
+                        <img 
+                        onClick={handDropDownToggle}
+                        src={user?.profileImage || avatarImg} alt="" className="size-6 rounded-full cursor-pointer" />
+                        {
+                            IsDropDownOpen && (
+                                <div className="absolute right-0 mt-3 p-4 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-50">
+                                    <ul className="font-medim space-y-4 p-2">
+                                        {dropdownMenus.map((menu, index) => (
+                                            <li key={index}>
+                                                <Link 
+                                                onClick={() => setIsDropDownOpen(false)}
+                                                className="dropdown-items"
+                                                to={menu.path}>{menu.label}</Link>
+                                            </li>
+                                        ))}
+                                        <li><Link onClick={handleLogout} className="dropdown-items">Logout</Link></li>
+                                    </ul>
+                                </div>
+                            )
+                        }
+                        
+                        </>) : (<Link to="login">
+                            <i className="ri-user-line"></i>
+                        </Link>)
+                    }
                 </span>
             </div>
         </nav>
